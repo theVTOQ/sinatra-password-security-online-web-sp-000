9eab3998ad072c21c51df746889bd2af3421a77e
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-29 00:08:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,7 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		#your code here!
+			user = User.find_by(username: params[:username], password: params[:password])
 	end
 
 	get "/success" do
